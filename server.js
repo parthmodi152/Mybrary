a9a1bc6232c92323f876629a04af437da41292c3
parthmodi152 <modi.parth152@gmail.com>
2020-12-23 20:24:57 +0530

    project setup

---
+++ server.js
@@ -0,0 +1,23 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname+'/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use('/', indexRouter);
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection
+db.on('error', err => { console.log(err) });
+db.once('open', err => { console.log(`Conected to Mongoose`) });
+
+app.listen(process.env.PORT || 3000)
